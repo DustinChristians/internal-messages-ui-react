f4656f18f27105ff68ebec888ef661d5f27f3933
Dustin <dustinchristians@gmail.com>
2020-04-01 19:09:15 -0400

    Added an input form to the messages page.

--- src/components/Messages/MessagesPage.js
+++ src/components/Messages/MessagesPage.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
-function MessagesPage() {
-  return (
-    <div>
-      <h2>Messages</h2>
-    </div>
-  );
-}
+const handleSubmit = (event) => {
+  // prevent the browser's default behavior to reload
+  event.preventDefault();
+  alert('submit');
+};
+
+const MessagesPage = () => (
+  <form onSubmit={handleSubmit}>
+    <h2>Messages</h2>
+    <h3>Add message</h3>
+    <input type="text" />
+    <input type="submit" value="Save" />
+  </form>
+);
 
 export default MessagesPage;
