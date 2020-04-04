336b8dcac363fa5ac4f37a98c75e2ef10a69a355
Dustin <dustinchristians@gmail.com>
2020-04-04 08:12:07 -0400

    Added Redux for creating messages on the messages page.

--- src/components/Messages/MessagesPage.js
+++ src/components/Messages/MessagesPage.js
@@ -1,18 +1,41 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import createMessage from '../../redux/actions/messageActions';
 
-const handleSubmit = (event) => {
-  // prevent the browser's default behavior to reload
-  event.preventDefault();
-  alert('submit');
+const MessagesPage = (props) => {
+  const { messages, dispatch } = props;
+
+  const handleSubmit = (event) => {
+    const message = { text: event.target.elements.message.value };
+
+    // prevent the browser's default behavior to reload
+    event.preventDefault();
+    dispatch(createMessage(message));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Messages</h2>
+      <h3>Add message</h3>
+      <input type="text" name="message" />
+      <input type="submit" value="Save" />
+      {messages.map((message) => (
+        <div key={message.text}>{message.text}</div>
+      ))}
+    </form>
+  );
 };
 
-const MessagesPage = () => (
-  <form onSubmit={handleSubmit}>
-    <h2>Messages</h2>
-    <h3>Add message</h3>
-    <input type="text" />
-    <input type="submit" value="Save" />
-  </form>
-);
+MessagesPage.propTypes = {
+  messages: PropTypes.instanceOf(Array).isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
 
-export default MessagesPage;
+function mapStateToProps(state) {
+  return {
+    messages: state.messages,
+  };
+}
+
+export default connect(mapStateToProps)(MessagesPage);
