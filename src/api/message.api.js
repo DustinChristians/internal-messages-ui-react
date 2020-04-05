b7abb6c597e3b37636381a97d9374f15efa3f918
Dustin <dustinchristians@gmail.com>
2020-04-05 07:55:58 -0400

    Fixed the .env environment variables for the API.

--- src/api/message.api.js
+++ src/api/message.api.js
@@ -1,6 +1,6 @@
 import { handleResponse, handleError } from './api.utils';
 
-const baseUrl = `${process.env.API_URL}/messages/`;
+const baseUrl = `${process.env.REACT_APP_API_URL}/messages/`;
 
 export function getMessages() {
   return fetch(baseUrl).then(handleResponse).catch(handleError);
--- src/api/user.api.js
+++ src/api/user.api.js
@@ -1,6 +1,6 @@
 import { handleResponse, handleError } from './api.utils';
 
-const baseUrl = `${process.env.API_URL}/users/`;
+const baseUrl = `${process.env.REACT_APP_API_URL}/users/`;
 
 export function getUsers() {
   return fetch(baseUrl).then(handleResponse).catch(handleError);
