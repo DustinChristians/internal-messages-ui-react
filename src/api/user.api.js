d813f89b73ec05cd519007b4cd4943e14feb5035
Dustin <dustinchristians@gmail.com>
2020-04-05 05:51:52 -0400

    Added the mock user and message APIs.

---
+++ src/api/user.api.js
@@ -0,0 +1,7 @@
+import { handleResponse, handleError } from './api.utils';
+
+const baseUrl = `${process.env.API_URL}/users/`;
+
+export function getUsers() {
+  return fetch(baseUrl).then(handleResponse).catch(handleError);
+}
