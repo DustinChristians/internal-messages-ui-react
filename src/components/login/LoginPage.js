ef97aa3055bc646cb2d8aef3776931d214a69d98
Dustin <dustinchristians@gmail.com>
2020-04-25 10:15:46 -0400

    Updated the login form.

--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -1,9 +1,44 @@
 import React from 'react';
 
 const LoginPage = () => (
-  <div>
-    <h2>Login</h2>
-  </div>
+  <form className="form-signin">
+    <div className="form-label-group">
+      <label htmlFor="username">
+        Username{' '}
+        <input
+          type="text"
+          id="username"
+          className="form-control"
+          name="username"
+          placeholder="Username"
+          required
+        />
+      </label>
+    </div>
+    <div className="form-label-group">
+      <label htmlFor="inputPassword">
+        Password{' '}
+        <input
+          type="password"
+          id="password"
+          className="form-control"
+          placeholder="Password"
+          required
+        />
+      </label>
+    </div>
+
+    <div className="custom-control custom-checkbox mb-3">
+      <label className="custom-control-label" htmlFor="customCheck1">
+        Remember password
+        <input type="checkbox" className="custom-control-input" id="customCheck1" />
+      </label>
+    </div>
+
+    <button className="btn btn-lg btn-primary btn-block text-uppercase" type="submit">
+      Sign in
+    </button>
+  </form>
 );
 
 export default LoginPage;
