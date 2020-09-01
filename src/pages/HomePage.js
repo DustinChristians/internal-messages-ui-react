12646a683938ce7deebe211d77cc2e195e19ba3d
Dustin <dustinchristians@gmail.com>
2020-09-01 18:52:00 -0400

    Reworked the home page into the chat application home page.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,42 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../logo.svg';
-import '../App.scss';
 
-function HomePage() {
+const HomePage = () => {
+  function handleClick() {
+    console.log('handleClick');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <ul>
-          <li>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </li>
-          <li>
-            <Link to="/messages" className="App-link">
-              API / Redux Test Page
-            </Link>{' '}
-            (to run the JSON-Server mock API use: <b>npm run start:dev</b>)
-          </li>
-          <li>
-            <a href={process.env.REACT_APP_API_URL} className="App-link">
-              JSON-Server Home Page
-            </a>{' '}
-          </li>
-        </ul>
-      </header>
-    </div>
+    <>
+      <div className="jumbotron">
+        <h1>Instant Chat</h1>
+        <p>Free conversations. Invite anyone.</p>
+      </div>
+
+      <Link className="btn btn-pink" role="button" to="/messages" onClick={handleClick}>
+        Start a Conversation
+      </Link>
+      <button className="btn btn-pink" type="button" onClick={handleClick}>
+        Join a Conversation
+      </button>
+    </>
   );
-}
+};
 
 export default HomePage;
